cd2a84e6d933c4e61e0de4150cd552de77f2adc0
rfmineguy <rfmineguy@gmail.com>
2023-08-10 14:26:10 -0700

    Working launchable state, with modid changed to 'item_transmutation'

---
+++ src/main/java/github/rfmineguy/item_transmutation/ItemTransmutationDataGenerator.java
@@ -0,0 +1,11 @@
+package github.rfmineguy.item_transmutation;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ItemTransmutationDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
--- src/main/java/github/rfmineguy/item_transmutation/mixin/ExampleMixin.java
+++ src/main/java/github/rfmineguy/item_transmutation/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package github.rfmineguy.item_transmutation.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
